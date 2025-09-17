36c95089067dc29179d8b81e13dca658c10271e0
陈为熙 <2504597640@qq.com>
2025-09-17 11:34:36 +0800

    feat: 接入监控系统

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { DndProvider } from "vue3-dnd";
 import RyButton from "@/components/CommonComponents/RyButton.vue";
 // 如果您正在使用CDN引入，请删除下面一行。
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import webSee from "@websee/core";
+import performance from "@websee/performance";
+import recordscreen from "@websee/recordscreen";
 
 const app = createApp(App);
 app
@@ -19,3 +22,13 @@ app
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 	app.component(key, component);
 }
+
+app.use(webSee, {
+	dsn: "http://172.16.202.228:8083/reportData",
+	apikey: "project1",
+	userId: "89757",
+	silentWhiteScreen: true,
+});
+
+webSee.use(performance);
+webSee.use(recordscreen);
